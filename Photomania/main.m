359e0c75b3d611874f1f8cc33939ce2bcd3becb6
m2m tech <tech@m2m.at>
2013-03-09 19:29:03 +0100

    Initial Commit

---
+++ Photomania/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Photomania
+//
+//  Created by Martin Mandl on 09.03.13.
+//  Copyright (c) 2013 m2m server software gmbh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
